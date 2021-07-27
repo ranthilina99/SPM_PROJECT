01c64afbc7807569cfa8a935f6c6fcc9061565d3
ranthilina99 <umeshranthilina111@gmail.com>
2021-07-27 19:58:37 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true
+
+}, (error) => {
+  if (error) {
+    console.log('Database Error: ', error.message);
+  }
+});
+
+mongoose.connection.once('open', () => {
+  console.log('Database Connected');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on PORT ${PORT}`);
+});
