239a03633d0fbd4622cb340782fe63a98417a3fb
shehanblast <shihanbinuka@gmail.com>
2021-09-09 23:33:42 +0530

    Add new route paths,Screens,Cart Header

--- client/src/Components/routes/routes.js
+++ client/src/Components/routes/routes.js
@@ -48,6 +48,9 @@ import EmpViewStore from "../forms/views/empViewStore";
 import EditStore from "../forms/Store/editStore";
 import CustomerViewStore from "../forms/views/customerViewStore";
 import StoreItem from "../forms/views/StoreItem";
+import HomeScreen from "../../screens/HomeScreen";
+import CartScreen from "../../screens/CartScreen";
+import ProductScreen from "../../screens/ProductScreen";
 
 function Routes() {
     return (
@@ -106,6 +109,9 @@ function Routes() {
                             <Route path="/empViewStore" component={EmpViewStore} />
                             <Route path="/editStore/:id" component={EditStore} />
                             <Route path="/storeItem/:id" component={StoreItem} />
+                            <Route exact path="/homeStore" component={HomeScreen} />
+                            <Route exact path="/product/:id" component={ProductScreen} />
+                            <Route exact path="/cart" component={CartScreen} />
                         </Route>
                     </Switch>
                 </Router>
